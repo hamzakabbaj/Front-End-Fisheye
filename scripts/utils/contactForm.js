8de42850c46fd8a4de2cd36d89eb0a13c8b1c20b
Hamza Kabbaj <hamza.kabbaj@live.fr>
2025-05-19 21:00:52 +0100

    Enhance contact form modal functionality by adding focus to the firstname input field after the modal is displayed. This improvement aims to streamline user interaction and improve accessibility.

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,12 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "block";
+  setTimeout(() => {
+    document.getElementById("firstname").focus();
+  }, 10);
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "none";
 }
